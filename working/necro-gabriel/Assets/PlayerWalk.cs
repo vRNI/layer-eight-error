507c1876b7fb147a154fe960d103f5820ed07b47
Gabriel Koidl <gkoidl.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 11:06:17 +0200

    Added rudimentary movement for our player caracter

---
+++ working/necro-gabriel/Assets/PlayerWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerWalk : MonoBehaviour {
+
+    private NavMeshAgent navMeshAgent;
+    private RaycastHit hitInformation;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInformation))
+            {
+                navMeshAgent.SetDestination(hitInformation.point);
+            }
+        }
+	}
+}
---
+++ working/necro-gabriel/Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log( "Start" );
+    }
+
+    void Update()
+    {
+        Debug.Log( "Update" );
+    }
+}
